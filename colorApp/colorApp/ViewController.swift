61a486a6a4092ea6b9f4e2aa3854d8aa773c6f02
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-10-04 16:38:20 -0400

    added table view for colors 60 rows

--- colorApp/colorApp/ViewController.swift
+++ colorApp/colorApp/ViewController.swift
@@ -14,11 +14,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func goToColors(_ sender: UIButton) {
-        performSegue(withIdentifier: "toColors", sender: nil)
-    }
+//    @IBAction func goToColors(_ sender: UIButton) {
+//        performSegue(withIdentifier: "toColors", sender: nil)
+//    }
     
     
 //    override func performSegue(withIdentifier identifier: "toColors", sender: nil;)
 }
 
+extension ViewController: UITableViewDelegate , UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 60
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        performSegue(withIdentifier: "toColors", sender: nil)
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toColors", sender: nil)
+    }
+}
